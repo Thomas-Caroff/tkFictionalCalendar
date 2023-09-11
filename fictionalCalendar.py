f035ac78c5da0e8f5a12ed88495614c1415b2191
thomas.caroff <[thomasc97@hotmail.fr]>
2023-09-11 19:35:30 +0200

    Changed dimensions, added intermediate frame

--- fictionalCalendar.py
+++ fictionalCalendar.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
 
-class MyComponent(tk.Frame):
+class Calendar(tk.Frame):
     def __init__(self, master, title, subtitle):
         super().__init__(master)
         self.master = master
@@ -23,8 +25,8 @@ class MyComponent(tk.Frame):
             row_frame.pack()
 
             for col in range(num_cols):
-                button = tk.Button(row_frame, text=str(current_num))
-                button.pack(side=tk.LEFT)
+                button = tk.Button(row_frame, text=str(current_num), width=9, height=4)
+                button.pack(side=tk.LEFT, padx=3, pady=3)
                 current_num += 1
                 
                 if (max_days !=0) and current_num > max_days:
@@ -32,12 +34,23 @@ class MyComponent(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Mon Composant tkinter")
+    root.title("Fictionnal calendar")
+    
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
 
-    title_text = "Titre de mon composant"
-    subtitle_text = "Sous-titre de mon composant"
+    root.geometry(f"{screen_width}x{screen_height}")
 
-    component = MyComponent(root, title_text, subtitle_text)
-    component.pack()
+    title_text = "Année"
+    subtitle_text = "Mois"
+    
+    width = 960
+    height = 540
+    
+    frame = tk.Frame(width=width, height=height, borderwidth=4, relief="sunken")
+    frame.place(anchor="nw")
+
+    component = Calendar(frame, title_text, subtitle_text)
+    component.pack(fill=tk.BOTH, expand=True)
 
     root.mainloop()
